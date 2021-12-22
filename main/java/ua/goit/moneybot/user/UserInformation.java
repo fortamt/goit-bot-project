f580c1c397598506a9003aba8e86342687ba66ee
19Den <zdenisv85@gmail.com>
2021-12-22 17:48:17 +0200

    collection, add, update, gson V4.1

--- main/java/ua/goit/moneybot/user/UserInformation.java
+++ main/java/ua/goit/moneybot/user/UserInformation.java
@@ -2,13 +2,13 @@ package ua.goit.moneybot.user;
 
 public class UserInformation {
     String name = "User";
-    int chatId = 1;
+    long chatId = 1;
     UserSettings userSettings = new UserSettings();
 
     public UserInformation() {
     }
 
-    public UserInformation(String name, int chatId, UserSettings userSettings) {
+    public UserInformation(String name, long chatId, UserSettings userSettings) {
         this.name = name;
         this.chatId = chatId;
         this.userSettings = userSettings;
