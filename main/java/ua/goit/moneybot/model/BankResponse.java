6bfdb905517f2eabca72495367950e8fc55e6896
IhorBeztsennyi <89686065+IhorBeztsennyi@users.noreply.github.com>
2021-12-23 17:02:59 +0200

    Update BankResponse.java

--- main/java/ua/goit/moneybot/model/BankResponse.java
+++ main/java/ua/goit/moneybot/model/BankResponse.java
@@ -3,90 +3,49 @@ package ua.goit.moneybot.model;
 import java.io.IOException;
 import java.math.BigDecimal;
 
-public class BankResponse{
+public class BankResponse {
 
-    private BigDecimal usdBuy;
-    private BigDecimal usdSell;
-    private BigDecimal eurBuy;
-    private BigDecimal eurSell;
-    private BigDecimal rubBuy;
-    private BigDecimal rubSell;
+    private String bankName;
+    private String currency;
+    private BigDecimal buyRate;
+    private BigDecimal sellRate;
 
-
-    private void setCurrency(UserSettings userSettings) throws IOException, InterruptedException {
-        if (userSettings.getBankName().equals("Monobank")){
-            MonobankAPI monobankAPI = new MonobankAPI();
-            this.usdBuy = monobankAPI.getUsdBuy();
-            this.usdSell = monobankAPI.getUsdSell();
-            this.eurBuy = monobankAPI.getEurBuy();
-            this.eurSell = monobankAPI.getEurSell();
-            this.rubBuy = monobankAPI.getRubBuy();
-            this.rubSell = monobankAPI.getRubSell();
-        } else if (userSettings.getBankName().equals("PrivatBank")){
-            PrivatBankankAPI privatBankankAPI = new PrivatBankankAPI();
-            this.usdBuy = privatBankankAPI.getUsdBuy();
-            this.usdSell = privatBankankAPI.getUsdSell();
-            this.eurBuy = privatBankankAPI.getEurBuy();
-            this.eurSell = privatBankankAPI.getEurSell();
-            this.rubBuy = privatBankankAPI.getRubBuy();
-            this.rubSell = privatBankankAPI.getRubSell();
-        } else if (userSettings.getBankName().equals("NBU")){
-            NBUAPI NBUAPI = new NBUAPI();
-            this.usdBuy = NBUAPI.getUsdBuy();
-            this.usdSell = NBUAPI.getUsdSell();
-            this.eurBuy = NBUAPI.getEurBuy();
-            this.eurSell = NBUAPI.getEurSell();
-            this.rubBuy = NBUAPI.getRubBuy();
-            this.rubSell = NBUAPI.getRubSell();
-        }
+    public BankResponse(String bankName, String currency, BigDecimal buyRate, BigDecimal sellRate) {
+        this.bankName = bankName;
+        this.currency = currency;
+        this.buyRate = buyRate;
+        this.sellRate = sellRate;
     }
 
-
-    public BigDecimal getUsdBuy() {
-        return usdBuy;
+    public String getBankName() {
+        return bankName;
     }
 
-    public void setUsdBuy(BigDecimal usdBuy) {
-        this.usdBuy = usdBuy;
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
     }
 
-    public BigDecimal getUsdSell() {
-        return usdSell;
+    public String getCurrency() {
+        return currency;
     }
 
-    public void setUsdSell(BigDecimal usdSell) {
-        this.usdSell = usdSell;
+    public void setCurrency(String currency) {
+        this.currency = currency;
     }
 
-    public BigDecimal getEurBuy() {
-        return eurBuy;
+    public BigDecimal getBuyRate() {
+        return buyRate;
     }
 
-    public void setEurBuy(BigDecimal eurBuy) {
-        this.eurBuy = eurBuy;
+    public void setBuyRate(BigDecimal buyRate) {
+        this.buyRate = buyRate;
     }
 
-    public BigDecimal getEurSell() {
-        return eurSell;
+    public BigDecimal getSellRate() {
+        return sellRate;
     }
 
-    public void setEurSell(BigDecimal eurSell) {
-        this.eurSell = eurSell;
-    }
-
-    public BigDecimal getRubBuy() {
-        return rubBuy;
-    }
-
-    public void setRubBuy(BigDecimal rubBuy) {
-        this.rubBuy = rubBuy;
-    }
-
-    public BigDecimal getRubSell() {
-        return rubSell;
-    }
-
-    public void setRubSell(BigDecimal rubSell) {
-        this.rubSell = rubSell;
+    public void setSellRate(BigDecimal sellRate) {
+        this.sellRate = sellRate;
     }
 }
