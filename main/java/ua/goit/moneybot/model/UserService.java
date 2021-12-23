a8172e39631279ff35e0a7a231930edbf73d4583
19Den <zdenisv85@gmail.com>
2021-12-23 13:31:21 +0200

    add methods to class UserService V4.4

--- main/java/ua/goit/moneybot/model/UserService.java
+++ main/java/ua/goit/moneybot/model/UserService.java
@@ -1,7 +1,6 @@
 package ua.goit.moneybot.model;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import org.telegram.telegrambots.meta.api.objects.Message;
 
 
@@ -9,12 +8,31 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class UserService {
-    private final Gson GSON = new Gson();
+    //private final Gson GSON = new Gson();
 
     Map<Long, UserSettings> users = new HashMap<>();
 
-    public void addUser(Message message, UserSettings userSettings){
+    public void addUser(Message message, UserSettings userSettings) {
         users.put(message.getChatId(), userSettings);
+    }
+
+    public void changeBank(Message message, String selectedBank) {
+        users.get(message).setSelectedBank(selectedBank);
+    }
+
+    public void changeRounding(Message message, byte digitAfterComa) {
+        users.get(message).setDigitAfterComa(digitAfterComa);
+    }
+
+    public void changeCurrency(Message message, boolean usd, boolean eur, boolean rub) {
+        users.get(message).setUsd(usd);
+        users.get(message).setUsd(eur);
+        users.get(message).setUsd(rub);
+    }
+
+    public void editTimeReminder(Message message, boolean notification, byte notificationTime) {
+        if (notification) users.get(message).setNotification(false);
+        else users.get(message).setNotificationTime(notificationTime);
     }
 
 
